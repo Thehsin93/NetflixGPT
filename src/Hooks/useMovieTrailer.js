8a413cd87074fea623d47232fd4168c66ba3e453
Thehsin93 <thehsinnew@gmail.com>
2024-05-14 11:33:04 +0400

    Updated buildf

--- src/Hooks/useMovieTrailer.js
+++ src/Hooks/useMovieTrailer.js
@@ -2,9 +2,10 @@ import { useDispatch } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
 import { API_options } from "../utils/constants";
 import { useEffect } from "react";
+import { useSelector } from "react-redux";
 const useMovieTrailer = (movieId)=>{
     const dispatch = useDispatch();
-   
+   const trailer = useSelector(store=>store.movies.trailerVideo);
     const getMovieVideo = async()=>{
         const data =await  fetch('https://api.themoviedb.org/3/movie/'+movieId+'/videos?language=en-US', API_options)
         const json = await data.json();
@@ -14,7 +15,7 @@ const useMovieTrailer = (movieId)=>{
         dispatch(addTrailerVideo(trailer));
     }
     useEffect(()=>{
-        getMovieVideo();
+        !trailer && getMovieVideo();
     },[]);
 };
 export default useMovieTrailer
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA6YqA2NN-L50DZhrSlf8uYxVUHfyw9z8o",
+  apiKey: process.env.REACT_APP_FIREBASEKEY,
   authDomain: "netflix-gpt-6aec9.firebaseapp.com",
   projectId: "netflix-gpt-6aec9",
   storageBucket: "netflix-gpt-6aec9.appspot.com",
